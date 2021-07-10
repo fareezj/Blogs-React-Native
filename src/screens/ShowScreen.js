7650fbf886575856b677a14bc13218488f993133
UF-Fareez <fareez.mohamad@hexainitiative.com>
2021-07-10 13:54:10 +0800

    Revert "Update prettier"
    This reverts commit 265b26dfb63588f58ccb4fb3d3ca39f309f94530.

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,37 +1,35 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { Context } from "../context/BlogContext";
-import { EvilIcons } from "@expo/vector-icons";
+import { EvilIcons } from '@expo/vector-icons';
 
-const ShowScreen = ({ navigation }) => {
- const { state } = useContext(Context);
+const ShowScreen = ({navigation}) => {
 
- const blogPost = state.find(
-  (blogPost) => blogPost.id == navigation.getParam("id")
- );
+    const {state} = useContext(Context);
 
- return (
-  <View>
-   <Text>Hello Fareez</Text>
-   <Text>{blogPost.title}</Text>
-   <Text>{blogPost.content}</Text>
-  </View>
- );
-};
+    const blogPost = state.find((blogPost) => blogPost.id == navigation.getParam('id'));
 
-ShowScreen.navigationOptions = ({ navigation }) => {
- return {
-  headerRight: () => (
-   <TouchableOpacity
-    onPress={() =>
-     navigation.navigate("Edit", { id: navigation.getParam("id") })
-    }
-   >
-    <EvilIcons name="pencil" size={35} />
-   </TouchableOpacity>
-  ),
- };
-};
+    return (
+        <View>
+            <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
+        </View>
+    )
+}
+
+ShowScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: () => 
+            <TouchableOpacity onPress={
+                () => navigation.navigate('Edit', 
+                {id: navigation.getParam('id')}
+                )}>
+                    
+                <EvilIcons name='pencil' size={35} />
+            </TouchableOpacity>
+        
+    };
+}
 
 const styles = StyleSheet.create({});
 
